31b49b288f20c62e921f335d7d7ac6382ee2ba0e
Brendan Dunn Kane <brendankane04@gmail.com>
2021-08-30 22:44:33 -0400

    Quality of life improvements were made.

--- blink/main/blink.c
+++ blink/main/blink.c
@@ -15,27 +15,79 @@
 /* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
    or you can edit the following line and set a number here.
 */
-#define BLINK_GPIO 33
+#define BLINK_GPIO GPIO_NUM_33
+
+char sbt0811_in[4] =
+{
+		GPIO_NUM_2,
+		GPIO_NUM_14,
+		GPIO_NUM_15,
+		GPIO_NUM_13
+};   // IN1, IN2, IN3, IN4 to arduino pins
+
+int full_steps_count	= 4095;    // steps for a full cycle
+const int step_time		= 1;       // 1ms for one step
+
+int direction    = 1;           // will be either 1 (clockwise) or -1 (anti-clockwise)
+int current_step = 0;           // 0 to 7
+
+bool step_positions[8][4] =
+{
+    { 1,0,0,1 },
+    { 1,0,0,0 },
+    { 1,1,0,0 },
+    { 0,1,0,0 },
+    { 0,1,1,0 },
+    { 0,0,1,0 },
+    { 0,0,1,1 },
+    { 0,0,0,1 },
+};
+
+void move_stepper(int step_count)
+{
+    for (int i=0;i<step_count;i++)
+    {
+        // step one step in given direction
+        current_step += direction;
+        if (current_step > 7) { current_step = 0; }
+        if (current_step < 0) { current_step = 7; }
+
+        // set digital pins for the current_step
+        for (int i = 0; i <= 3; i++)
+        {
+            gpio_set_level(sbt0811_in[i], step_positions[current_step][i]);
+        }
+
+        vTaskDelay(step_time / portTICK_PERIOD_MS);
+    }
+}
 
 void app_main(void)
 {
-    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
-       muxed to GPIO on reset already, but some default to other
-       functions and need to be switched to GPIO. Consult the
-       Technical Reference for a list of pads and their default
-       functions.)
-    */
-    gpio_reset_pin(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-    while(1) {
-        /* Blink off (output low) */
-        printf("Turning off the LED\n");
-        gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        /* Blink on (output high) */
-        printf("Turning on the LED\n");
-        gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+	for (int i = 0; i <= 3; i++)
+	{
+	        gpio_reset_pin(sbt0811_in[i]);
+	        gpio_set_direction(sbt0811_in[i], GPIO_MODE_OUTPUT);
+	}
+
+	int steps_left   = full_steps_count;
+
+	printf("Setup Completed.\n\n\n");
+
+    while(1)
+    {
+
+    	while (steps_left > 0)
+    	{
+    	         move_stepper(direction);
+    	         steps_left -= 1;
+    	}
+    	steps_left = full_steps_count;
+    	direction  = (direction > 0 ? -1 : 1);
+
+    	vTaskDelay(500 / portTICK_PERIOD_MS);
+
+        printf("Stepping Process Completed\n");
     }
 }
+
