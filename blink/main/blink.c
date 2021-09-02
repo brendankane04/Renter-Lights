625e15a8868bc543a690a6b234a21e71cdb39bd2
Brendan Dunn Kane <brendankane04@gmail.com>
2021-09-01 23:56:02 -0400

    Servo control was added.

--- blink/main/blink.c
+++ blink/main/blink.c
@@ -9,7 +9,10 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_attr.h"
 #include "driver/gpio.h"
+#include "driver/mcpwm.h"
+#include "soc/mcpwm_periph.h"
 #include "sdkconfig.h"
 
 /* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
@@ -18,83 +21,64 @@
 #define BLINK_GPIO GPIO_NUM_33
 #define delay(cnt) vTaskDelay(cnt / portTICK_PERIOD_MS)
 
-char sbt0811_in[4] =
+//You can get these value from the datasheet of servo you use, in general pulse width varies between 1000 to 2000 mocrosecond
+#define SERVO_MIN_PULSEWIDTH 1000 //Minimum pulse width in microsecond
+#define SERVO_MAX_PULSEWIDTH 2000 //Maximum pulse width in microsecond
+#define SERVO_MAX_DEGREE 90 //Maximum angle in degree upto which servo can rotate
+
+static void mcpwm_example_gpio_initialize(void)
 {
-		GPIO_NUM_2,
-		GPIO_NUM_14,
-		GPIO_NUM_15,
-		GPIO_NUM_13
-};   // IN1, IN2, IN3, IN4 to arduino pins
+    printf("initializing mcpwm servo control gpio......\n");
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, 18);    //Set GPIO 18 as PWM0A, to which servo is connected
+}
 
-int full_steps_count	= 4095;    // steps for a full cycle
-const int step_time		= 1;       // 1ms for one step
-
-int direction    = 1;           // will be either 1 (clockwise) or -1 (anti-clockwise)
-int current_step = 0;           // 0 to 7
-
-bool step_positions[8][4] =
+/**
+ * @brief Use this function to calcute pulse width for per degree rotation
+ *
+ * @param  degree_of_rotation the angle in degree to which servo has to rotate
+ *
+ * @return
+ *     - calculated pulse width
+ */
+static uint32_t servo_per_degree_init(uint32_t degree_of_rotation)
 {
-    { 1,0,0,1 },
-    { 1,0,0,0 },
-    { 1,1,0,0 },
-    { 0,1,0,0 },
-    { 0,1,1,0 },
-    { 0,0,1,0 },
-    { 0,0,1,1 },
-    { 0,0,0,1 },
-};
+    uint32_t cal_pulsewidth = 0;
+    cal_pulsewidth = (SERVO_MIN_PULSEWIDTH + (((SERVO_MAX_PULSEWIDTH - SERVO_MIN_PULSEWIDTH) * (degree_of_rotation)) / (SERVO_MAX_DEGREE)));
+    return cal_pulsewidth;
+}
 
-void move_stepper(int step_count)
+/**
+ * @brief Configure MCPWM module
+ */
+void mcpwm_example_servo_control(void *arg)
 {
-    for (int i=0;i<step_count;i++)
-    {
-        // step one step in given direction
-        current_step += direction;
-        if (current_step > 7) { current_step = 0; }
-        if (current_step < 0) { current_step = 7; }
+    uint32_t angle, count;
+    //1. mcpwm gpio initialization
+    mcpwm_example_gpio_initialize();
 
-        // set digital pins for the current_step
-        for (int i = 0; i <= 3; i++)
-        {
-            gpio_set_level(sbt0811_in[i], step_positions[current_step][i]);
+    //2. initial mcpwm configuration
+    printf("Configuring Initial Parameters of mcpwm......\n");
+    mcpwm_config_t pwm_config;
+    pwm_config.frequency = 50;    //frequency = 50Hz, i.e. for every servo motor time period should be 20ms
+    pwm_config.cmpr_a = 0;    //duty cycle of PWMxA = 0
+    pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
+    pwm_config.counter_mode = MCPWM_UP_COUNTER;
+    pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);    //Configure PWM0A & PWM0B with above settings
+    while (1) {
+        for (count = 0; count < SERVO_MAX_DEGREE; count++) {
+            printf("Angle of rotation: %d\n", count);
+            angle = servo_per_degree_init(count);
+            printf("pulse width: %dus\n", angle);
+            mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, angle);
+            vTaskDelay(10);     //Add delay, since it takes time for servo to rotate, generally 100ms/60degree rotation at 5V
         }
-
-        delay(step_time);
     }
 }
 
 void app_main(void)
 {
-	for (int i = 0; i <= 3; i++)
-	{
-	        gpio_reset_pin(sbt0811_in[i]);
-	        gpio_set_direction(sbt0811_in[i], GPIO_MODE_OUTPUT);
-	}
-
-	int state = 1;
-	gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-
-	int steps_left   = full_steps_count;
-
-	printf("Setup Completed.\n\n\n");
-
-    while(1)
-    {
-
-    	while (steps_left > 0)
-    	{
-    	         move_stepper(direction);
-    	         steps_left -= 1;
-    	}
-    	steps_left = full_steps_count;
-    	direction  = (direction > 0 ? -1 : 1);
-
-    	delay(500);
-
-    	gpio_set_level(BLINK_GPIO, state);
-    	state = !state;
-
-        printf("Stepping Process Completed\n");
-    }
+    printf("Testing servo motor.......\n");
+    xTaskCreate(mcpwm_example_servo_control, "mcpwm_example_servo_control", 4096, NULL, 5, NULL);
 }
 
