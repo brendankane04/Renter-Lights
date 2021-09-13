7df1202433cbc3ce2da8e875f47a42cf55b2c78e
Brendan Dunn Kane <brendankane04@gmail.com>
2021-09-12 23:03:28 -0400

    The wifi driver was developed.

--- main/Wifi_Interface.cpp
+++ main/Wifi_Interface.cpp
@@ -4,3 +4,35 @@
  *  Created on: Sep 8, 2021
  *      Author: brend
  */
+
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#include "Wifi_Interface.h"
+
+Wifi_Interface::Wifi_Interface()
+{
+	esp_err_t err;
+
+	//Initialize the netif interface
+	err = esp_netif_init();
+	ESP_ERROR_CHECK(err);
+
+	//Initialize the event loop
+	err = esp_event_loop_create_default();
+	ESP_ERROR_CHECK(err);
+
+	//Initialize the wifi system
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	err = esp_wifi_init(&cfg);
+	ESP_ERROR_CHECK(err);
+}
---
+++ main/Wifi_Interface.h
@@ -0,0 +1,25 @@
+/*
+ * Wifi_Interface.h
+ *
+ *  Created on: Sep 12, 2021
+ *      Author: brend
+ */
+
+#ifndef MAIN_WIFI_INTERFACE_H_
+#define MAIN_WIFI_INTERFACE_H_
+
+class Wifi_Interface
+{
+	private:
+
+	public:
+
+	private:
+
+	public:
+		Wifi_Interface();
+};
+
+
+
+#endif /* MAIN_WIFI_INTERFACE_H_ */
