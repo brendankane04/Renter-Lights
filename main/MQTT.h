4dbd1ba5e04fc010db5da22dcd9867111047d08f
Brendan Dunn Kane <brendankane04@gmail.com>
2022-07-08 22:23:22 -0400

    MQTT is working

---
+++ main/MQTT.h
@@ -0,0 +1,21 @@
+/*
+ * MQTT.h
+ *
+ *  Created on: Jul 7, 2022
+ *      Author: brend
+ */
+
+#ifndef MQTT_H_
+#define MQTT_H_
+
+
+#include "mqtt_client.h"
+
+
+#define CONFIG_BROKER_URL "mqtt://testserver.local:1883"
+
+
+void mqtt_init();
+
+
+#endif /* MAIN_MQTT_H_ */
--- main/main.cpp
+++ main/main.cpp
@@ -9,8 +9,8 @@
 #include "sdkconfig.h"
 #include "SG90.h"
 #include "SR501.h"
+#include "MQTT.h"
 #include "Wifi_Interface.h"
-#include "mqtt_client.h"
 
 
 #define delay(cnt) vTaskDelay(cnt / portTICK_PERIOD_MS)
@@ -27,7 +27,6 @@ static const char *TAG = "main";
 #define TARGET_PORT 21
 #define OPERATING_MODE 2
 #define TEST_STR "TEST STRING. If you're seeing this, the refactor worked!\n"
-#define CONFIG_BROKER_URL "mqtt://testserver.local:1883"
 
 
 extern "C" { void app_main(); }
@@ -43,98 +42,10 @@ void blink()
 }
 
 
-//<MQTT>
-
-
-static void log_error_if_nonzero(const char *message, int error_code)
-{
-    if (error_code != 0) {
-        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
-    }
-}
-
-static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
-{
-    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
-    esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t) event_data;
-    esp_mqtt_client_handle_t client = event->client;
-    int msg_id;
-    switch((esp_mqtt_event_id_t)event_id)
-    {
-		case MQTT_EVENT_CONNECTED:
-			ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-			msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
-			ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
-
-			msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
-			ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-			msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
-			ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-			msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
-			ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
-		break;
-		case MQTT_EVENT_DISCONNECTED:
-			ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-		break;
-
-		case MQTT_EVENT_SUBSCRIBED:
-			ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-			msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
-			ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
-		break;
-		case MQTT_EVENT_UNSUBSCRIBED:
-			ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
-		break;
-		case MQTT_EVENT_PUBLISHED:
-			ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
-		break;
-		case MQTT_EVENT_DATA:
-			ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-			printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-			printf("DATA=%.*s\r\n", event->data_len, event->data);
-		break;
-		case MQTT_EVENT_ERROR:
-			ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-			if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT)
-			{
-				log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
-				log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
-				log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
-				ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
-
-			}
-		break;
-		default:
-			ESP_LOGI(TAG, "Other event id:%d", event->event_id);
-		break;
-    }
-}
-
-//MQTT setup
-void mqtt_init()
-{
-	esp_mqtt_client_config_t mqtt_cfg =
-	{
-		.uri = CONFIG_BROKER_URL,
-	};
-
-	esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
-	/* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
-	esp_mqtt_client_register_event(client, MQTT_EVENT_ANY, mqtt_event_handler, NULL);
-	esp_mqtt_client_start(client);
-}
-
-
-//</MQTT>
-
-
 //Send signals to the network based on the input
 //Uncalled by the user, just used by the internals
 void populated_signal_handler(void* handler_arg, esp_event_base_t base, int32_t id, void* event_data)
 {
-//	Wifi_Interface wifi = Wifi_Interface::get_instance("Home Network", "ThanksBrendan!");
     wifi.set_target(TARGET_IP, TARGET_PORT);
 
 	switch(id)
@@ -214,10 +125,12 @@ void app_main(void)
 	//Initialize the wifi
 	wifi.init("Home Network", "ThanksBrendan!");
     wifi.set_target(TARGET_IP, TARGET_PORT);
-//    SG90 servo(SERVO_SIG_GPIO);
+    SG90 servo(SERVO_SIG_GPIO);
 
 	gpio_pad_select_gpio(BLINK_GPIO);
 	gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+	mqtt_init();
 
 	switch(OPERATING_MODE)
 	{
